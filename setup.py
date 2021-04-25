78d402e2d2b0b02731672747bb30d66accbbc4b8
TeU-Lin <trusaidlin@gmail.com>
2021-04-26 00:54:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="auvio",
-    version="1.0.1",
+    version="1.0.2",
     author="NPC OwO",
     author_email="support@ntut.club",
     description="Let your zuvio roll the call automatically, "
